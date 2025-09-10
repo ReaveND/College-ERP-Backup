bcfc6866802e8385a5be0f6dd5f90325a7ca8983
GitHub Action <action@github.com>
2025-09-10 07:39:05 +0000

    Backup from College-ERP on Wed Sep 10 07:39:05 UTC 2025

---
+++ backups/2025-09-10/App/src/components/StudentData.jsx
@@ -0,0 +1,19 @@
+import ProfilePic from '../assets/pfp.jpg';
+
+const StudentData = {
+  fullName: "Srabani Kar",
+  rollNumber: "MCA2025-017",
+  regNumber: "MCA25-0010",
+  department: "Masters of Computer Application",
+  profilePicture: ProfilePic,
+  email: "srabanikar05@gmail.com",
+  phone: "+91-9064285877",
+  dob: "2002-07-25",
+  semester: "3rd",
+  enrollmentYear: "2024",
+  guardianName: "Mr. Biswapati Kar",
+  guardianPhone: "+91-9831902492",
+  idCardUrl: "/docs/id-card.pdf",
+};
+
+export default StudentData;
